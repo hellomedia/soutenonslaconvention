45207ba0f136b91826808f0cdb2e69694b205ba7
Olly Cope <olly@ollycope.com>
2020-06-17 18:26:33 +0000

    migrations: add more oauth2 profile columns

--- migrations/20200617_01_bkBbe-create-initial-schema.sql
+++ migrations/20200617_01_bkBbe-create-initial-schema.sql
@@ -2,14 +2,16 @@
 -- depends: 
 
 
-CREATE OR REPLACE TABLE supporters (
+CREATE TABLE supporters (
     id SERIAL,
     email TEXT UNIQUE,
     social_provider TEXT,
     social_id TEXT UNIQUE,
     picture_url TEXT,
+    full_name TEXT,
     first_name TEXT,
     last_name TEXT,
+    locale TEXT,
     account_confirmed BOOL NOT NULL DEFAULT false,
     PRIMARY KEY(id)
 );
