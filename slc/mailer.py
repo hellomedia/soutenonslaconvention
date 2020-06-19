c857b7636e501429c5955f296462bbb77792c632
Olly Cope <olly@ollycope.com>
2020-06-19 14:35:52 +0000

    Add support for multiple smtp backends

--- slc/mailer.py
+++ slc/mailer.py
@@ -1,4 +1,14 @@
 from mailsend import Mail
+from random import choices
 from slc import options
 
-mailer = Mail(sendmail="sendmail", **options.MAILER_OPTIONS)
+mailers = []
+mailer_weights = []
+for item in options.SMTP_SERVERS:
+    if item["enabled"]:
+        mailers.append(Mail(item["url"], **options.MAILER_OPTIONS))
+        mailer_weights.append(item["monthly_quota"])
+
+
+def get_mailer():
+    return choices(mailers, weights=mailer_weights, k=1)[0]
--- slc/queuing.py
+++ slc/queuing.py
@@ -1,10 +1,16 @@
-from huey import SqliteHuey
-from slc.appbootstrap import options
-from slc.mailer import mailer
+import logging
 
+from huey import SqliteHuey
+
+from slc.appbootstrap import options
+from slc.mailer import get_mailer
+
+logger = logging.getLogger(__name__)
 huey = SqliteHuey(filename=options.HUEY_DB)
 
 
 @huey.task(retries=10, retry_delay=60)
 def queue_send_mail(*args, **kwargs):
+    mailer = get_mailer()
+    logger.warning(f"Sending via {mailer.username} @ {mailer.hostname}")
     mailer.send(*args, **kwargs)
