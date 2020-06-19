8ad9bf89e80195d4d37e9b64e1ee68d9107e0f6a
Olly Cope <olly@ollycope.com>
2020-06-19 19:36:35 +0000

    Fix sql typo

--- slc/queries/update_supporter_profile.sql
+++ slc/queries/update_supporter_profile.sql
@@ -3,6 +3,6 @@ SET
     display_name = COALESCE(:display_name, display_name),
     reason = COALESCE(:reason, reason),
     suggestion = COALESCE(:suggestion, suggestion),
-    image_path = COALESCE(:image_path, image_path)
+    image_path = COALESCE(:image_path, image_path),
     display_image = COALESCE(:display_image, display_image)
 WHERE id = :id
