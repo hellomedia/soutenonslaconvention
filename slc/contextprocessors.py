fdc5fc8b18dfb33c9770b0690b16fb666a80a4ae
Olly Cope <olly@ollycope.com>
2020-06-21 15:26:52 +0000

    templating: add markdown rendering support

--- slc/contextprocessors.py
+++ slc/contextprocessors.py
@@ -1,10 +1,21 @@
+import functools
+from textwrap import dedent
+
 from fresco_i18n.i18nformat import Formatter
 from fresco_flash import flash
+from markupsafe import Markup
 
 from slc import assets
 from slc.templating import piglet
 
 formatter = Formatter("fr")
+
+
+@functools.lru_cache()
+def render_markdown(s):
+    from markdown import Markdown
+
+    return Markup(Markdown().convert(dedent(s)))
 
 
 @piglet.contextprocessor
@@ -13,5 +24,6 @@ def default_context(request):
         "static": lambda path: f"/static/{path}",
         "assets": assets.assetbuilder,
         "format": formatter,
+        "markdown": render_markdown,
         "flash_messages": flash.messages(),
     }
