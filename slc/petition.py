71eab1624e556d3e6c40ea6a6fa6a9e3195b957e
Olly Cope <olly@ollycope.com>
2020-06-22 10:43:08 +0000

    Add module to fetch petition count from mesopinions.fr

---
+++ slc/petition.py
@@ -0,0 +1,45 @@
+import re
+
+from returns.io import IOResultE
+from returns.io import impure_safe
+from returns.result import safe
+from returns.unsafe import unsafe_perform_io
+import html5lib
+import requests
+
+
+@impure_safe
+def fetch_petition_page() -> requests.Response:
+    r = requests.get(
+        "https://www.mesopinions.com/"
+        "petition/politique/"
+        "sortie-crise-soutenons-convention-citoyenne-climat/86134"
+    )
+    r.raise_for_status
+    return r
+
+
+@safe
+def parse_count(r: requests.Response) -> int:
+    doc = html5lib.parse(r.text)
+    print(r.text)
+    text = doc.find(".//*[@class='mo-counter hide-sm']/{*}strong").text
+    return int(re.sub(r"[^\d]", "", text))
+
+
+def fetch_count() -> IOResultE:
+    return fetch_petition_page().bind(parse_count)
+
+
+def get_cached_count() -> int:
+    from slc.caching import get_or_create
+
+    def get_value():
+        value = unsafe_perform_io(fetch_count())
+        if isinstance(value, Exception):
+            raise value
+        return value
+
+    return get_or_create(
+        "petition-count", get_value, hours=1, use_stale_on_error=True
+    )
